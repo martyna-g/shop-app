e3b5c26278cc13e279701123ef754d098ffae980
Martyna Gorna <mart.gorna@gmail.com>
2022-02-26 16:38:55 +0000

    Add DISCOUNT_RATE filed to Product class and final keyword to setters

--- src/com/tinks/data/Product.java
+++ src/com/tinks/data/Product.java
@@ -1,17 +1,19 @@
 package com.tinks.data;
 
 import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
 
 public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(final int id) {
         this.id = id;
     }
 
@@ -19,7 +21,7 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
@@ -27,7 +29,11 @@ public class Product {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
     }
 }
