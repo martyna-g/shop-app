05fc217c1d8206f2a27ac71866d956f7be1a921e
Martyna Gorna <mart.gorna@gmail.com>
2022-02-28 13:32:40 +0000

    Create constructors for Food and Drink classes

--- src/com/tinks/data/Drink.java
+++ src/com/tinks/data/Drink.java
@@ -1,4 +1,10 @@
 package com.tinks.data;
 
+import java.math.BigDecimal;
+
 public class Drink extends Product {
+
+    public Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
 }
--- src/com/tinks/data/Food.java
+++ src/com/tinks/data/Food.java
@@ -1,4 +1,18 @@
 package com.tinks.data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Food extends Product {
+
+    private LocalDate bestBefore;
+
+    public Food(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
+        super(id, name, price, rating);
+        this.bestBefore = bestBefore;
+    }
+
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
 }
