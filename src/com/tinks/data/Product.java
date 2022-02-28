bf077cfca27d5668c188bf8cefced5b01fb06e03
Martyna Gorna <mart.gorna@gmail.com>
2022-02-28 09:54:22 +0000

    Remove all setter methods from the Product class

--- src/com/tinks/data/Product.java
+++ src/com/tinks/data/Product.java
@@ -30,24 +30,12 @@ public class Product {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 
     public BigDecimal getDiscount() {
