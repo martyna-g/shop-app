a0355248e8dce16b0ccec3a60337efe66aab581c
Martyna Gorna <mart.gorna@gmail.com>
2022-03-01 14:29:38 +0000

    Create class Review, add constructor which setes all of it's attributes and add getters

---
+++ src/com/tinks/data/Review.java
@@ -0,0 +1,20 @@
+package com.tinks.data;
+
+public class Review {
+
+    private Rating rating;
+    private String comments;
+
+    public Review(Rating rating, String comments) {
+        this.rating = rating;
+        this.comments = comments;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+}
