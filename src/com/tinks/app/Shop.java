9fcf3faf854691a61025640ee0b11ed243691941
Martyna Gorna <mart.gorna@gmail.com>
2022-03-01 08:45:45 +0000

    Create instance of ProductManager and use it to create objects of Product

--- src/com/tinks/app/Shop.java
+++ src/com/tinks/app/Shop.java
@@ -1,17 +1,19 @@
 package com.tinks.app;
 
-import com.tinks.data.Product;
-import com.tinks.data.Rating;
-import java.math.BigDecimal;
+import com.tinks.data.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 
 public class Shop {
     public static void main(String[] args) {
-        Product p1 = new Product(101, "Tea", BigDecimal.valueOf(1.99));
-        Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-        Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR);
-        Product p4 = new Product();
+        ProductManager pm = new ProductManager();
+
+        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
+        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now().plusDays(2));
 
         System.out.println(p1);
         System.out.println(p2);
