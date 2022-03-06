2562a27443d62c46bd78c3cb5c7177f19651cd88
Martyna Gorna <mart.gorna@gmail.com>
2022-03-06 10:43:02 +0000

    Replace all loops that process products and reviews collections with Streams

--- src/com/tinks/data/ProductManager.java
+++ src/com/tinks/data/ProductManager.java
@@ -7,6 +7,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ProductManager {
 
@@ -55,24 +57,26 @@ public class ProductManager {
         List<Review> reviews = products.get(product);
         products.remove(product, reviews);
         reviews.add(new Review(rating, comments));
-        int sum = 0;
-        for (Review review : reviews) {
-            sum += review.getRating().ordinal();
-        }
-        product = product.applyRating(Rateable.convert(Math.round((float) sum / reviews.size())));
+        product = product.applyRating(
+                Rateable.convert(
+                        (int)Math.round(
+                                reviews.stream()
+                                        .mapToInt(r -> r.getRating().ordinal())
+                                        .average()
+                                        .orElse(0)
+                        )
+                )
+        );
         products.put(product, reviews);
         return product;
     }
 
     public Product findProduct(int id) {
-        Product result = null;
-        for (Product product : products.keySet()) {
-            if (product.getId() == id) {
-                result = product;
-                break;
-            }
-        }
-        return result;
+        return products.keySet()
+                .stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseGet(() -> null);
     }
 
     public void printProductReport(int id) {
@@ -85,25 +89,23 @@ public class ProductManager {
         txt.append(formatter.formatProduct(product));
         txt.append('\n');
         Collections.sort(reviews);
-        for (Review review : reviews) {
-            txt.append(formatter.formatReviews(review));
-            txt.append('\n');
-        }
         if (reviews.isEmpty()) {
-            txt.append(formatter.getText("no.reviews"));
-            txt.append('\n');
+            txt.append(formatter.getText("no.reviews") + '\n');
+        } else {
+            txt.append(reviews.stream()
+                    .map(r -> formatter.formatReviews(r) + '\n')
+                    .collect(Collectors.joining()));
         }
         System.out.println(txt);
     }
 
-    public void printProducts(Comparator<Product> sorter) {
-        List<Product> productList = new ArrayList<>((products.keySet()));
-        productList.sort(sorter);
+    public void printProducts(Predicate<Product> filter, Comparator<Product> sorter) {
         StringBuilder txt = new StringBuilder();
-        for (Product product : productList) {
-            txt.append(formatter.formatProduct(product));
-            txt.append('\n');
-        }
+        products.keySet()
+                .stream()
+                .sorted(sorter)
+                .filter(filter)
+                .forEach(p -> txt.append(formatter.formatProduct(p) + '\n'));
         System.out.println(txt);
     }
 
