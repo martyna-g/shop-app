8220490451155f52dfa1ca2c27532fbc8b6afe1e
Martyna Gorna <mart.gorna@gmail.com>
2022-03-08 14:46:55 +0000

    Modify printProductReport method to produce text files containing formatted products and reviews

--- src/com/tinks/data/ProductManager.java
+++ src/com/tinks/data/ProductManager.java
@@ -1,6 +1,12 @@
 package com.tinks.data;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -21,6 +27,9 @@ public class ProductManager {
     private ResourceBundle config = ResourceBundle.getBundle("com.tinks.data.config");
     private MessageFormat reviewFormat = new MessageFormat(config.getString("review.data.format"));
     private MessageFormat productFormat = new MessageFormat(config.getString("product.data.format"));
+    private Path reportsFolder = Path.of(config.getString("reports.folder"));
+    private Path dataFolder = Path.of(config.getString("data.folder"));
+    private Path tempFolder = Path.of(config.getString("temp.folder"));
     private static Map<String, ResourceFormatter> formatters
             = Map.of("en-GB", new ResourceFormatter(Locale.UK),
             "en-US", new ResourceFormatter(Locale.US),
@@ -97,23 +106,23 @@ public class ProductManager {
             printProductReport(findProduct(id));
         } catch (ProductManagerException e) {
             logger.log(Level.INFO, e.getMessage());
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error printing product report " + e.getMessage(), e);
         }
     }
 
-    public void printProductReport(Product product) {
+    public void printProductReport(Product product) throws IOException {
         List<Review> reviews = products.get(product);
-        StringBuilder txt = new StringBuilder();
-        txt.append(formatter.formatProduct(product));
-        txt.append('\n');
         Collections.sort(reviews);
-        if (reviews.isEmpty()) {
-            txt.append(formatter.getText("no.reviews") + '\n');
-        } else {
-            txt.append(reviews.stream()
-                    .map(r -> formatter.formatReviews(r) + '\n')
-                    .collect(Collectors.joining()));
+        Path productFile = reportsFolder.resolve(MessageFormat.format(config.getString("report.file"), product.getId()));
+        try (PrintWriter out = new PrintWriter(new OutputStreamWriter(Files.newOutputStream(productFile, StandardOpenOption.CREATE), "UTF-8"))) {
+            out.append(formatter.formatProduct(product) + System.lineSeparator());
+            if (reviews.isEmpty()) {
+                out.append(formatter.getText("no.reviews") + System.lineSeparator());
+            } else {
+                out.append(reviews.stream().map(r -> formatter.formatReviews(r) + System.lineSeparator()).collect(Collectors.joining()));
+            }
         }
-        System.out.println(txt);
     }
 
     public void printProducts(Predicate<Product> filter, Comparator<Product> sorter) {
