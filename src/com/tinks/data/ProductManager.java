e00becb08db5e73a8f26540216488f9e74232c01
Martyna Gorna <mart.gorna@gmail.com>
2022-03-09 10:24:07 +0000

    Implement memory swap mechanism - write logic that dumps all product and review objects into temprorary swap file and restore objects from this file back into memory

--- src/com/tinks/data/ProductManager.java
+++ src/com/tinks/data/ProductManager.java
@@ -1,8 +1,6 @@
 package com.tinks.data;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.math.BigDecimal;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -11,6 +9,7 @@ import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -146,6 +145,36 @@ public class ProductManager {
             logger.log(Level.WARNING, "Error parsing review " + text, e);
         }
         return review;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void restoreData() {
+        try {
+            Path tempFile = Files.list(tempFolder)
+                    .filter(path -> path.getFileName().toString().endsWith("tmp"))
+                    .findFirst()
+                    .orElseThrow();
+            try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(tempFile, StandardOpenOption.DELETE_ON_CLOSE))) {
+                products = (HashMap) in.readObject();
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error restoring data " + e.getMessage(), e);
+        }
+    }
+
+    public void dumpData() {
+        try {
+            if (Files.notExists(tempFolder)) {
+                Files.createDirectory(tempFolder);
+            }
+            Path tempFile = tempFolder.resolve(MessageFormat.format(config.getString("temp.file"), Instant.now()));
+            try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(tempFile, StandardOpenOption.CREATE))) {
+                out.writeObject(products);
+                products = new HashMap<>();
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error dumping data " + e.getMessage(), e);
+        }
     }
 
     private void loadAllData() {
