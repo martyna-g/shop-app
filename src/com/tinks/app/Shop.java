816e17c8d6893ed119600a87a3cef08b6bbcb7de
Martyna Gorna <mart.gorna@gmail.com>
2022-03-10 08:18:51 +0000

    Write code to simulate concurrent callers

--- src/com/tinks/app/Shop.java
+++ src/com/tinks/app/Shop.java
@@ -4,20 +4,58 @@ import com.tinks.data.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Shop {
     public static void main(String[] args) {
-        ProductManager pm = new ProductManager();
+        ProductManager pm = ProductManager.getInstance();
+        AtomicInteger clientCount = new AtomicInteger(0);
 
-        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
-        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now().plusDays(2));
+        Callable<String> client = () -> {
+            String clientId = "Client " + clientCount.incrementAndGet();
+            String threadName = Thread.currentThread().getName();
+            int productId = ThreadLocalRandom.current().nextInt(63) + 101;
+            String languageTag = ProductManager.getSupportedLocales()
+                    .stream()
+                    .skip(ThreadLocalRandom.current().nextInt(3))
+                    .findFirst().get();
+            StringBuilder log = new StringBuilder();
+            log.append(clientId + " " + threadName + "\n-\tstart of log\t-\n");
+            log.append(pm.getDiscounts(languageTag)
+                    .entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() + "\t" + entry.getValue())
+                    .collect(Collectors.joining("\n")));
+            Product product = pm.reviewProduct(productId, Rating.FOUR_STAR, "Yet another review");
+            log.append((product != null) ? "\nProduct " + productId + " reviewed\n" : "\nProduct " + productId + " not reviewed\n");
+            pm.printProductReport(productId, languageTag, clientId);
+            log.append(clientId + " generated report for " + productId + " product");
+            log.append("\n-\tend of log\t-\n");
+            return log.toString();
+        };
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
+        List<Callable<String>> clients = Stream.generate(() -> client).limit(5).collect(Collectors.toList());
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        try {
+            List<Future<String>> results = executorService.invokeAll(clients);
+            executorService.shutdown();
+            results.stream().forEach(result -> {
+                try {
+                    System.out.println(result.get());
+                } catch (InterruptedException | ExecutionException e ) {
+                    Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, "Error retrieving client log", e);
+                }
+            });
+        } catch (InterruptedException e) {
+            Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, "Error invoking clients", e);
+        }
     }
 }
